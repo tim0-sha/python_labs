f479cec6004b5560ad2b45ecfc64d3a758cdf72a
tim0-sha <tim0sha07@yandex.ru>
2026-09-15 14:00:55 +0300

    0

--- src/lab01/04_minutes_to_hhmm.py
+++ src/lab01/04_minutes_to_hhmm.py
@@ -1,2 +1,4 @@
 m=int(input("Минуты: "))
+days = m // 1440 
+m = m - days * 1440 
 print(f"{m//60}:{m%60:02d}")
--- src/lab01/ex06.py
+++ src/lab01/ex06.py
@@ -1,8 +1,8 @@
-n=int(input())
+n=int(input("in_1: "))
 ochno=0;zaochno=0
 for i in range(n):
-    s=input().split()
+    s=input(f"in_{i+2}: ").split()
     if s[-1]=="True":
         ochno+=1
     else: zaochno+=1
-print(ochno,zaochno)
+print("out: ",ochno,zaochno)
